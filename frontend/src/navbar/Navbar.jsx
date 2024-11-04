a461b1e570acf654fe1d3def69dad33c419f18e9
Roland550 <133446553+Roland550@users.noreply.github.com>
2024-11-04 13:36:40 +0100

    appjs fixed00

--- frontend/src/navbar/Navbar.jsx
+++ frontend/src/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
 import del from "../assets/delt.jpeg";
-import "./Navbar.css";
+import "./navbar.css";
 import { logout } from "../redux/user/userSlice";
 // import bell from '../assets/bell.png'
 
